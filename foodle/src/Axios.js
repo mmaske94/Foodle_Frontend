f638ddbff351baf66282043552e41f757e32121e
mmaske94 <mmaske94@outlook.com>
2022-09-15 12:52:30 -0400

    Heroku Fix

--- foodle/src/Axios.js
+++ foodle/src/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = (process.env.MONGODB || 'http://localhost:4000');
 axios.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
